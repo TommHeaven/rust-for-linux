3eb22fa8f98bff49533c5c292b2deef1d0a3bec9
Wedson Almeida Filho <wedsonaf@google.com>
2022-07-08 17:53:46 +0000

    rust: add support for file system parameters
    This allows file system contexts to be further initialised with
    parameters from userspace before a fs is mounted or reconfigured.
    
    Signed-off-by: Wedson Almeida Filho <wedsonaf@google.com>
    [Adapted for upstream, replaced UnsafeCell with Opaque, replaced
    from_kernel_result with from_result]
    Signed-off-by: Ariel Miculas <amiculas@cisco.com>

--- rust/kernel/lib.rs
+++ rust/kernel/lib.rs
@@ -19,6 +19,7 @@
 #![feature(offset_of)]
 #![feature(receiver_trait)]
 #![feature(unsize)]
+#![feature(const_mut_refs)]
 
 // Ensure conditional compilation based on the kernel configuration works;
 // otherwise we may silently break things like initcall handling.
